9082a5d91b5c840d967898fe92261b0cd2958a1d
zhuanghongji <zhuanghongjichina@gmail.com>
2018-11-24 17:19:22 +0800

    Initial Commit

---
+++ app/src/main/java/com/zhuanghongji/flowersview/FlowersView.java
@@ -0,0 +1,245 @@
+package com.zhuanghongji.flowersview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * 自定义 View : 繁华曲线
+ */
+public class FlowersView extends View {
+
+    private static final String TAG = "FlowersView";
+
+    private static final float COMPLETE_DEVIATION = 10F;
+
+    private Paint mAssistPaint;
+
+    private Paint mTargetPaint;
+
+    private Path mTargetPath;
+
+    private int mSideLength;
+
+    private float mRadian;
+    private Point mPointFirstDraw;
+
+    private float mRadiusA;
+    private Point mPointA;
+
+    private float mRadiusB;
+    private Point mPointB;
+
+    private float mRadiusC;
+    private Point mPointC;
+
+    private OnCompleteListener mOnCompleteListener;
+
+    public FlowersView(Context context) {
+        this(context, null);
+    }
+
+    public FlowersView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FlowersView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPaint();
+        initPoint();
+        mTargetPath = new Path();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        mSideLength = getMeasuredWidth();
+        setMeasuredDimension(mSideLength, mSideLength);
+
+        // （不变值）大圆半径为画布边长 1/4, 圆心在正中间
+        mRadiusA = mSideLength / 2;
+        mPointA.x = mSideLength / 2;
+        mPointA.y = mSideLength / 2;
+
+        // （默认值）小圆半径是边长 1/4, 圆心 x 轴是边长 1/4, y 轴是画布边长 1/2
+        changeRadiusB(mSideLength / 4);
+
+        mRadiusC = mSideLength / 8;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawTargetPath(canvas);
+        drawCircleA(canvas);
+        drawCircleB(canvas);
+        drawLine(canvas);
+    }
+
+    private void drawTargetPath(Canvas canvas) {
+        mTargetPath.reset();
+        boolean isHaveMoved = false;
+
+        double radian = 0;
+        while (radian < mRadian) {
+            radian += 0.01;
+            Point tempPointC = getPointInChildCircle(mPointA,
+                    mRadiusA, mRadiusB, mRadiusC, radian);
+
+            if (!isHaveMoved) {
+                mTargetPath.moveTo(tempPointC.getX(), tempPointC.getY());
+                isHaveMoved = true;
+            } else {
+                mTargetPath.lineTo(tempPointC.getX(), tempPointC.getY());
+            }
+        }
+        canvas.drawPath(mTargetPath, mTargetPaint);
+    }
+
+    private void drawLine(Canvas canvas) {
+        if (mPointC.x == 0 && mPointC.y == 0) {
+            return;
+        }
+        canvas.drawLine(mPointB.x, mPointB.y,
+                mPointC.x, mPointC.y, mAssistPaint);
+
+        mAssistPaint.setStrokeWidth(16);
+
+        canvas.drawPoint(mPointC.x, mPointC.y, mAssistPaint);
+        mAssistPaint.setStrokeWidth(2);
+    }
+
+    private void drawCircleB(Canvas canvas) {
+        canvas.drawCircle(mPointB.x, mPointB.y, mRadiusB, mAssistPaint);
+    }
+
+    private void drawCircleA(Canvas canvas) {
+        canvas.drawCircle(mPointA.x, mPointA.y, mRadiusA, mAssistPaint);
+    }
+
+    private void setupPointC() {
+        if (mRadian == 0) {
+            return;
+        }
+        mPointC = getPointInChildCircle(mPointA, mRadiusA, mRadiusB, mRadiusC, mRadian);
+    }
+
+    private void setupPointB() {
+        mPointB = getPointInCircle(mPointA, mRadiusA - mRadiusB, mRadian);
+    }
+
+    private void initPoint() {
+        mPointFirstDraw = new Point();
+
+        mPointA = new Point();
+        mPointB = new Point();
+        mPointC = new Point();
+    }
+
+    private void initPaint() {
+        mAssistPaint = new Paint();
+        mAssistPaint.setColor(Color.BLUE);
+        mAssistPaint.setStyle(Paint.Style.STROKE);
+        mAssistPaint.setStrokeWidth(2);
+        mAssistPaint.setStrokeCap(Paint.Cap.ROUND);
+        mAssistPaint.setAntiAlias(true);
+
+        mTargetPaint = new Paint();
+        mTargetPaint.setColor(Color.RED);
+        mTargetPaint.setStyle(Paint.Style.STROKE);
+        mTargetPaint.setStrokeWidth(6);
+        mTargetPaint.setAntiAlias(true);
+    }
+
+    public int getSideLength() {
+        return mSideLength;
+    }
+
+    /**
+     * 已知圆的圆心和半径，获取某弧度对应的圆上点的坐标
+     * @param center 圆心
+     * @param radius 半径
+     * @param radian 弧度
+     * @return
+     */
+    private Point getPointInCircle(Point center, double radius, double radian) {
+        float x = (float) (center.x + radius * Math.cos(radian));
+        float y = (float) (center.y - radius * Math.sin(radian));
+        return new Point(x, y);
+    }
+
+    private Point getPointInChildCircle(Point centerA, double radiusA,
+                                        double radiusB, double radiusC, double radian) {
+        Point centerB = getPointInCircle(centerA, radiusA - radiusB, radian);
+        double radianC = 2.0 * Math.PI - ((radiusA / radiusB * radian) % (2.0 * Math.PI));
+        return getPointInCircle(centerB, radiusC, radianC);
+    }
+
+    public void changeRadiusB(int radiusB) {
+        mRadiusB = radiusB;
+        setupPointB();
+        setupPointC();
+
+        mPointFirstDraw = getPointInChildCircle(mPointA, mRadiusA, mRadiusB, mRadiusC, 0.01);
+    }
+
+    public void changeRadiusC(int radiusC) {
+        mRadiusC = radiusC;
+        setupPointB();
+        setupPointC();
+
+        mPointFirstDraw = getPointInChildCircle(mPointA, mRadiusA, mRadiusB, mRadiusC, 0.01);
+    }
+
+    public void changeRadian(double radian) {
+        mRadian = (float) radian;
+        Log.d(TAG, "mRadian = " + mRadian);
+        setupPointB();
+        setupPointC();
+
+        if(Math.abs(mPointFirstDraw.x - mPointC.x) < COMPLETE_DEVIATION
+                && Math.abs(mPointFirstDraw.y - mPointC.y) < COMPLETE_DEVIATION
+                && radian > 10) {
+            if (mOnCompleteListener != null) {
+                mOnCompleteListener.onComplete();
+            }
+        }
+    }
+
+    public void setOnCompleteListener(OnCompleteListener onCompleteListener) {
+        mOnCompleteListener = onCompleteListener;
+    }
+
+    class Point {
+
+        float x;
+        float y;
+
+        Point() {}
+
+        Point(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        int getX() {
+            return (int) x;
+        }
+
+        int getY() {
+            return (int) y;
+        }
+    }
+
+    public interface OnCompleteListener {
+        void onComplete();
+    }
+
+}
---
+++ app/src/main/java/com/zhuanghongji/flowersview/MainActivity.java
@@ -0,0 +1,176 @@
+package com.zhuanghongji.flowersview;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    public static final int HANDLER_WHAT_START = 0;
+    public static final int HANDLER_WHAT_STOP = 1;
+
+    private static final int AUTO_INTERVAL = 1000;
+
+    private FlowersView mFlowersView;
+
+    private AppCompatSeekBar sbRadiusB;
+    private TextView tvRadiusB;
+
+    private AppCompatSeekBar sbRadiusC;
+    private TextView tvRadiusC;
+
+    private AppCompatSeekBar sbRadian;
+    private TextView tvRadian;
+
+    private int mRadian = 0;
+    private boolean mIsStart = false;
+    private TextView tvRadianChange;
+
+    private Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what == HANDLER_WHAT_START) {
+                mHandler.post(mAutoIncreaseRadian);
+                return true;
+            }
+            if (msg.what == HANDLER_WHAT_STOP) {
+                mHandler.removeCallbacksAndMessages(null);
+                return true;
+            }
+            return false;
+        }
+    });
+
+    private Runnable mAutoIncreaseRadian = new Runnable() {
+        @Override
+        public void run() {
+            mRadian++;
+            sbRadian.setProgress(mRadian);
+            mHandler.postDelayed(this, AUTO_INTERVAL);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initEvent();
+
+        mFlowersView.post(new Runnable() {
+            @Override
+            public void run() {
+                int radiusBMax = mFlowersView.getSideLength() / 2;
+                sbRadiusB.setMax(radiusBMax);
+                sbRadiusC.setMax(radiusBMax);
+
+//                int radiusCMax = mFlowersView.getSideLength() / 2;
+//                sbRadiusB.setMax(radiusCMax);
+
+                int bigRadianMax = 10000;
+                sbRadian.setMax(bigRadianMax);
+                tvRadian.setText(String.valueOf(bigRadianMax));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void initEvent() {
+        sbRadiusB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvRadiusB.setText(String.valueOf(progress));
+
+                int sbc = sbRadiusC.getProgress();
+                sbRadiusC.setMax(progress);
+                sbRadiusC.setProgress(sbc);
+
+                mFlowersView.changeRadiusB(progress);
+                mFlowersView.postInvalidate();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        sbRadiusC.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvRadiusC.setText(String.valueOf(progress));
+
+                mFlowersView.changeRadiusC(progress);
+                mFlowersView.postInvalidate();
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        sbRadian.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvRadian.setText(String.valueOf(progress));
+
+                mFlowersView.changeRadian(progress * 1.0 / 10);
+                mFlowersView.postInvalidate();
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        tvRadianChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mIsStart) {
+                    mRadian = 0;
+                    mIsStart = true;
+                    mHandler.sendEmptyMessage(HANDLER_WHAT_START);
+                    return;
+                }
+                mIsStart = false;
+                mHandler.sendEmptyMessage(HANDLER_WHAT_STOP);
+            }
+        });
+
+        mFlowersView.setOnCompleteListener(new FlowersView.OnCompleteListener() {
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete");
+                mHandler.sendEmptyMessage(HANDLER_WHAT_STOP);
+            }
+        });
+    }
+
+    private void initView() {
+        mFlowersView = findViewById(R.id.flowers_view);
+
+        sbRadiusB = findViewById(R.id.sb_radius_b);
+        tvRadiusB = findViewById(R.id.tv_radius_b);
+
+        sbRadiusC = findViewById(R.id.sb_radius_c);
+        tvRadiusC = findViewById(R.id.tv_radius_c);
+
+        sbRadian = findViewById(R.id.sb_radian);
+        tvRadian = findViewById(R.id.tv_radian);
+
+        tvRadianChange = findViewById(R.id.tv_radian_change);
+    }
+
+}
